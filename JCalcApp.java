82ced5ebf41e9083fdab378fb0da13860d243654
vincentja <jia.zhen@hongfu001.com>
2013-07-20 17:44:52 +0800

    JCalcApp

---
+++ JCalcApp.java
@@ -0,0 +1,8 @@
+
+public class JCalcApp {
+
+  public static void main(String[] args) {
+    JCalculatorApplication app = new JCalculatorApplication();
+    System.out.println("Started Calculator Application - waiting for messages");
+  }
+}
